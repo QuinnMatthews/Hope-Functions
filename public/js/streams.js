ec5484860e285aa5fcd68cb2b842369778f4b448
Matthews, Quinn <Quinn.Matthews@lrs.com>
2024-04-27 00:47:52 -0500

    append to `tbody`

--- public/js/streams.js
+++ public/js/streams.js
@@ -1,12 +1,13 @@
 // Populate Streams table with data from the server
 function populateStreamsTable() {
     var table = $('#streams-table');
+    var tbody = table.find('tbody');
     $.ajax({
         url: 'https://develop.hope-functions.pages.dev/RestreamChannels', // TODO: Change to your server URL
         type: 'GET',
         success: function(data) {
             data.forEach(function(stream) {
-                table.append(`
+                tbody.append(`
                 <tr class="border-b border-blue-gray-200">
                 <td class="py-3 px-4">${stream.name}</td>
                 <td class="py-3 px-4">${stream.platform.name}</td>

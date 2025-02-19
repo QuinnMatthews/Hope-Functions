c2230eff91c95f6ee8f4b340e6b4fad6f0d6c3c7
Matthews, Quinn <Quinn.Matthews@lrs.com>
2025-02-19 16:38:09 -0600

    Add skeleton to recieve cloudflare webhooks

---
+++ functions/OnCloudflareLiveEvent.ts
@@ -0,0 +1,21 @@
+export const onRequest: PagesFunction<Env> = async (context) => {
+    const request = context.request;
+    const headers = request.headers;
+
+    if (headers.get("cf-webhook-auth") !== context.env.AdminKey) {
+        return new Response(":(", {
+            headers: {'content-type': 'text/plain'},
+            status: 401
+        })
+    }
+
+    let incReq = await request.json()
+
+    // so we can see what we actually get sent.
+    console.log(incReq)
+
+    return new Response(":)", {
+        headers: {'content-type': 'text/plain'},
+    })
+
+};
